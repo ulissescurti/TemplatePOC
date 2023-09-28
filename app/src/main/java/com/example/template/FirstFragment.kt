999ec5084677a478ff9ce55b8955da81d5c7e849
Ulisses <ucurti@gmail.com>
2023-09-28 11:17:18 -0300

    Adding recycler view with mocked values and using it

--- app/src/main/java/com/example/template/FirstFragment.kt
+++ app/src/main/java/com/example/template/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.example.template
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.template.databinding.FragmentFirstBinding
 
@@ -14,31 +14,38 @@ import com.example.template.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private var adapter: ListAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        adapter = ListAdapter(getList()) {
+            this@FirstFragment.findNavController()
+                .navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        binding.recyclerView.adapter = adapter
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getList(): MutableList<String> {
+        val list = mutableListOf<String>()
+        for (i in 1..100) {
+            list.add("Item $i")
+        }
+        return list
     }
 }
