8001cb7ac14a19c8360f650f97e3bf2917c98e0a
Ulisses <ucurti@gmail.com>
2023-09-28 13:16:20 -0300

    minor adjustments

--- app/src/main/java/com/example/template/template/TemplateDataHelper.kt
+++ app/src/main/java/com/example/template/template/TemplateDataHelper.kt
@@ -3,14 +3,18 @@ package com.example.template.template
 object TemplateDataHelper {
     fun getMockedTemplateData(): TemplateData {
         val response = TemplateData(mutableListOf()).apply {
+            elements.add(TemplateText("Primeiro texto da lista de elementos"))
+            elements.add(
+                TemplateButton("Botão no começo", android.R.color.holo_blue_light)
+            )
             for (i in 1..50) {
                 val text = TemplateText("Text $i")
                 elements.add(text)
             }
             elements.add(
-                TemplateButton("Call to action", android.R.color.holo_red_light)
+                TemplateButton("Botão no final", android.R.color.holo_green_light)
             )
-            elements.add(TemplateText("Texto abaixo do botão"))
+            elements.add(TemplateText("Último elemento"))
         }
 
         return response
